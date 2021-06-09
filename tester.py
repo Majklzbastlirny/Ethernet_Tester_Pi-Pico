0daf77a0c441b4157dea1c3a89511d957abaf656
Majklzbastlirny <55651520+Majklzbastlirny@users.noreply.github.com>
2021-06-09 17:52:30 +0200

    Probably final release
    Schematic maybe added in some weeks

--- tester.py
+++ tester.py
@@ -12,13 +12,15 @@ led.low()
 I2C_ADDR= 0x3F
 I2C_NUM_ROWS = 4
 I2C_NUM_COLS = 20
-i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
+i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=4000000)
 lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+
+lcd.clear()
 
 
 an_sw1 = machine.ADC(26)
-an_sw2 = machine.ADC(27)
-an_sw3 = machine.ADC(28)
+#an_sw2 = machine.ADC(27)
+#an_sw3 = machine.ADC(28)
 
 sw1 = Pin(2, Pin.OUT)
 sw2 = Pin(3, Pin.OUT)
@@ -40,7 +42,7 @@ var_7 = 0
 var_8 = 0
 
 
-def clear():
+def clearall():
     sw1.low()
     sw2.low()
     sw3.low()
@@ -54,6 +56,8 @@ def clear():
     
 def get_1():
     sw1.low()
+    sw2.low()
+    sw3.low()
     time.sleep(0.25)
     var1 = an_sw1.read_u16()
     time.sleep(0.25)
@@ -62,108 +66,124 @@ def get_1():
 
 def get_2():
     sw1.high()
+    sw2.low()
+    sw3.low()
     time.sleep(0.25)
     var2 = an_sw1.read_u16()
     time.sleep(0.25)
-    sw1.low()
     print(var2)
     
 def get_3():
-    sw1.high()
+    sw1.low()
+    sw2.high()
+    sw3.low()
     time.sleep(0.25)
     var3 = an_sw1.read_u16()
     time.sleep(0.25)
-    sw1.low()
     print(var3)
 
 def get_4():
     sw1.high()
+    sw2.high()
+    sw3.low()
     time.sleep(0.25)
     var4 = an_sw1.read_u16()
     time.sleep(0.25)
-    sw1.low()
     print(var4)
     
 def get_5():
-    sw1.high()
+    sw1.low()
+    sw2.low()
+    sw3.high()
     time.sleep(0.25)
     var5 = an_sw1.read_u16()
     time.sleep(0.25)
-    sw1.low()
     print(var5)
     
 def get_6():
     sw1.high()
+    sw2.low()
+    sw3.high()
     time.sleep(0.25)
     var6 = an_sw1.read_u16()
     time.sleep(0.25)
-    sw1.low()
     print(var6)
     
 def get_7():
-    sw1.high()
+    sw1.low()
+    sw2.high()
+    sw3.high()
     time.sleep(0.25)
     var7 = an_sw1.read_u16()
     time.sleep(0.25)
-    sw1.low()
     print(var7)
     
 def get_8():
     sw1.high()
+    sw2.high()
+    sw3.high()
     time.sleep(0.25)
     var8 = an_sw1.read_u16()
     time.sleep(0.25)
-    sw1.low()
     print(var8)
     
     
 #zde bude lůp
-lcd.putstr("Vadim blyat")
+lcd.putstr("Ethernet tester     @Majklzbastlirny")
 time.sleep(2)
+clearall()
 lcd.clear()
 
 lcd.putstr("Merim drat 1")
 get_1()
 time.sleep(0.75)
+clearall()
 lcd.clear()
 
 lcd.putstr("Merim drat 2")
 get_2()
 time.sleep(0.75)
+clearall()
 lcd.clear()
 
 lcd.putstr("Merim drat 3")
 get_3()
 time.sleep(0.75)
+clearall()
 lcd.clear()
 
 lcd.putstr("Merim drat 4")
 get_4()
 time.sleep(0.75)
+clearall()
 lcd.clear()
 
 lcd.putstr("Merim drat 5")
 get_5()
 time.sleep(0.75)
+clearall()
 lcd.clear()
 
 lcd.putstr("Merim drat 6")
 get_6()
 time.sleep(0.75)
+clearall()
 lcd.clear()
 
 lcd.putstr("Merim drat 7")
 get_7()
 time.sleep(0.75)
+clearall()
 lcd.clear()
 
 lcd.putstr("Merim drat 8")
 get_8()
 time.sleep(0.75)
+clearall()
 lcd.clear()
 
 lcd.putstr("Zmereno")
-led.toggle()
+led.toggle()      
 utime.sleep(1)
 led.toggle()
 utime.sleep(0.2) 
